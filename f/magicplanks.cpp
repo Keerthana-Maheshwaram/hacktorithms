bacd792fcd512e70fb53d42d0e0d6a2c0ac69068
SithLord <92605352+s1th-lord@users.noreply.github.com>
2022-10-02 16:22:25 +0530

    Add files via upload

---
+++ f/magicplanks.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,a=0;
+        cin>>n;
+        string s;
+        cin>>s;
+        for(int i=1;i<n;i++)
+        {
+            if(s[i]==s[i-1])
+            a++;
+
+        }
+        
+    
+        
+    }
+    return 0;
+}
---
+++ f/tuple count.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long t,n,c=0;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        
+
+    }
+}
